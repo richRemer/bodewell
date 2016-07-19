6faef85e992d33f4cfd8a477a7a6b9acd1e34dfc
Richard Remer <rich@zingle.me>
2016-07-19 11:41:16 -0700

    support for dropping privileges

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ server = bode(process.env);
 server.listen(process.env.port || server.port, function() {
     var address = server.address();
     
+    if (process.env.user) {
+        console.log("dropping privileges");
+        process.setgid(process.env.group || process.env.user);
+        process.setuid(process.env.user);
+    }
+
     address = address.family === "IPv6"
         ? `[${address.address}]:${address.port}`
         : `${address.address}:${address.port}`;
